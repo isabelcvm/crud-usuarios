32b217bfed446b47156ba2cd33c7ebaeaff2433c
isabelcvm <isabelcvm259@gmail.com>
2023-03-10 12:12:19 -0400

    Creacion del resto de las vista relacionadas a toda la navegacion de la app

---
+++ src/components/ModalContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { DialogContentText } from '@mui/material'
+
+const ModalContent = () => {
+    return (
+        <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+        </DialogContentText>
+    )
+}
+
+export default ModalContent
